e838a142fd0528f82a2d8454284dabc95eea7db6
Prince Wiredu Darfour <daprinco@gmail.com>
2021-11-10 10:32:53 +0000

    Docs: Add credits to code

--- bikeshare.py
+++ bikeshare.py
@@ -1,3 +1,9 @@
+# Below are some of the links used in solving the project:
+# https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
+# https://stackoverflow.com/questions/2847386/python-string-and-integer-concatenation
+# https://stackoverflow.com/questions/19377969/combine-two-columns-of-text-in-dataframe-in-pandas-python
+
+
 import time
 import pandas as pd
 import numpy as np
